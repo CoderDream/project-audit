3d627fd1eee7aba7afdb2b8da7340ea8c6e50f51
CoderDream <coderdream@gmail.com>
2017-08-10 22:29:58 +0800

    修复项目查询历史信息未清空的缺陷

--- src/main/webapp/assets/js/query.js
+++ src/main/webapp/assets/js/query.js
@@ -31,6 +31,8 @@ $(function() {
 					html = $('<li data-val="' + i + '">' + projectName + '</li>');
 					html.data("_dataItem_",val);
 					$ul.append(html);
+					$("#projectId").val('');//清空填充内容 
+					$("#project").hide(); // 隐藏项目详情
 				});
 				//$ul.html(html);
 				$ul.listview("refresh");
@@ -53,6 +55,7 @@ $(function() {
 					// console.log(projectitem.projectName);
 					var key = $(this).attr("data-val");
 					var value = $(this).text();
+					$("#project").show(); // 显示项目详情
 					//$("#projectId").attr("value", key);//填充内容 
 					$("#projectId").val(key);//填充内容 
 					$("#autocomplete_input_project").val(value); 
@@ -91,66 +94,69 @@ $(function() {
 		var $ul = $(this),
 			$input = $(data.input),
 			value = $input.val();
-		$ul.html("");
-		var url = "/project/query";
-		$ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
-		$ul.listview("refresh");
-		$.ajax({
-			url: url,
-			dataType: "json",
-			crossDomain: false,
-			data: {
-				value: $input.val()
-			}
-		})
-		.then(function(response) {
-			var html;
+		if(value && value.length > 1) {
 			$ul.html("");
-			$.each(response, function(i, val) {
-				var projectName = val.projectName;
-				html = $('<li data-val="' + i + '">' + projectName + '</li>');
-				html.data("_dataItem_", val);
-				$ul.append(html);
-			});
-			html="";
-			//$ul.html(html);
+			var url = "/project/query";
+			$ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
 			$ul.listview("refresh");
-			$ul.trigger("updatelayout");
-			$("#autocomplete-query-by-project>li").on("click", function(e) {
-				var projectitem = $(this).data("_dataItem_");
-				var projectId = projectitem.id;
-				var projectName = projectitem.projectName;
-
-				var projectmgrWorkId = projectitem.projectmgrWorkId;
-				var projectmgrName = projectitem.projectmgrName;
-				var projectstartdatetime = new Date(projectitem.projectstartdatetime);
-				var projectenddatetime = new Date(projectitem.projectenddatetime);
-				var projectStartDateString = projectstartdatetime.formatToString("yyyy-MM-dd");
-				var projectEndDateString = projectenddatetime.formatToString("yyyy-MM-dd");
-				
-				// $("#contactsName").val('[' + projectmgrWorkId + ']' + projectmgrName);
-				$("#projectName").text('['+projectmgrWorkId+']'+projectmgrName);
-				$("#projectStartDateString").text(projectStartDateString);
-				$("#projectEndDateString").text(projectEndDateString);
-				
-				// console.log(projectitem.projectName);
-				var key = $(this).attr("data-val");
-				var value = $(this).text();
-				//$("#projectId").attr("value", key);//填充内容 
-				$("#projectId").val(key);//填充内容 
-				$("#autocomplete_input_project").val(value); 
-				$("#project").show(); // 显示项目详情
-				// console.log($("#projectId").val());
-				// alert($("#projectId").val());
-				// 点击后，关闭弹出框
-				html = "";
-				$ul.html(html);
+			$.ajax({
+				url: url,
+				dataType: "json",
+				crossDomain: false,
+				data: {
+					value: $input.val()
+				}
+			})
+			.then(function(response) {
+				var html;
+				$ul.html("");
+				$.each(response, function(i, val) {
+					var projectName = val.projectName;
+					html = $('<li data-val="' + i + '">' + projectName + '</li>');
+					html.data("_dataItem_", val);
+					$ul.append(html);
+					$("#projectId").val('');//清空填充内容 
+					$("#project").hide(); // 隐藏项目详情
+				});
+				html="";
+				//$ul.html(html);
 				$ul.listview("refresh");
 				$ul.trigger("updatelayout");
-			});
-		});
-	});  
+				$("#autocomplete-query-by-project>li").on("click", function(e) {
+					var projectitem = $(this).data("_dataItem_");
+					var projectId = projectitem.id;
+					var projectName = projectitem.projectName;
 	
+					var projectmgrWorkId = projectitem.projectmgrWorkId;
+					var projectmgrName = projectitem.projectmgrName;
+					var projectstartdatetime = new Date(projectitem.projectstartdatetime);
+					var projectenddatetime = new Date(projectitem.projectenddatetime);
+					var projectStartDateString = projectstartdatetime.formatToString("yyyy-MM-dd");
+					var projectEndDateString = projectenddatetime.formatToString("yyyy-MM-dd");
+					
+					// $("#contactsName").val('[' + projectmgrWorkId + ']' + projectmgrName);
+					$("#projectName").text('['+projectmgrWorkId+']'+projectmgrName);
+					$("#projectStartDateString").text(projectStartDateString);
+					$("#projectEndDateString").text(projectEndDateString);
+					
+					// console.log(projectitem.projectName);
+					var key = $(this).attr("data-val");
+					var value = $(this).text();
+					//$("#projectId").attr("value", key);//填充内容 
+					$("#projectId").val(key);//填充内容 
+					$("#autocomplete_input_project").val(value); 
+					$("#project").show(); // 显示项目详情
+					// console.log($("#projectId").val());
+					// alert($("#projectId").val());
+					// 点击后，关闭弹出框
+					html = "";
+					$ul.html(html);
+					$ul.listview("refresh");
+					$ul.trigger("updatelayout");
+				});
+			});
+		}
+	});  
 	
 	$("#project-btn").on("click", function() {
 		var projectId = $("#projectId").val();
